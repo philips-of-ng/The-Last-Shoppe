4d93a4a2315c37a8a31aea3b841f9109847ac392
philips-of-ng@philips <philipsedun@gmail.com>
2025-06-04 12:35:10 +0100

    this app has been updated lol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,49 @@
 import { Routes, Route } from 'react-router-dom';
 import Start from './pages/Start';
-// import Home from './pages/Home';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { AuthContext } from './context/AuthContext';
-// import View from './pages/View';
 import AdminDashboard from './pages/AdminDashboard';
 import Home from './pages/Home';
+
+// Component Testing
 import OneProduct from './components/OneProduct';
+import { products } from './assets/assets/assets';
+
 function App() {
 
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    document.title = 'Final Project'
+    document.title = 'Final Project';
     console.log('This is the user from Auth Context', user);
-  })
+  }, [user]);
+
+  const testProduct = products.slice(0, 1)[0]
+  console.log('These are the assets imported in the APP.JSX file', testProduct);
 
   const RouteHandler = ({ user }) => {
-    if (!user) return <Start />
-    if (user.isAdmin) return <AdminDashboard />
-    return <Home />
-  }
+    if (!user) return <Start />;
+    if (user.isAdmin) return <AdminDashboard />;
+    return <Home />;
+  };
 
   return (
-    <Routes>
-    
-      //This is the original code
-      {/* <Route path='/*' element={<RouteHandler user={user} />} /> */}
+    <>
+      <Routes>
+        {/* This is the original code */}
+        {/* <Route path='/*' element={<RouteHandler user={user} />} /> */}
 
+        {/* This is just for testing components */}
+        <Route path='/*' element={<OneProduct product={testProduct} />} />
+      </Routes>
 
-      //This is just for testing components
-
-      { 
-        <Route path='/*' element={<OneProduct />} />
-      }
-    </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
 export default App;
-
--- frontend/src/components/OneProduct.jsx
+++ frontend/src/components/OneProduct.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import 
+import React, { useEffect } from 'react'
+import '../css/one-product.css'
 
 const OneProduct = ({ product }) => {
-  return (
-    <div>
-      <div>
 
+  useEffect(() => {
+    console.log('This is the test product', product);
+  }, [])
+
+
+  return (
+    <div className='one-product-page'>
+
+      <img src={product.image[0]} alt="" />
+
+      <div className='price-n-descr'>
+        <div>
+          <h2 className='fw-bold'>$17.99</h2>
+        </div>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nostrum soluta ab ducimus excepturi hic rem earum, reiciendis quia quis.</p>
       </div>
-      This is one product page
+
 
 
     </div>
